c06a645c30d4a4c941d49df4b59ebe71ad4c0dd3
Leroy2flex <ngomaleroy@gmail.com>
2023-03-13 06:19:39 -0700

    static

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,12 +1,17 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * main - Entry point
+ * _puts - prints a string followed by a new line, to stdout
+ * @str: input variable
  *
- * Return: Always 0 (Success)
+ * Return: void
  */
-int main(void)
+
+void _puts(char *str)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	while (*str != '\0')
+	{
+		_putchar (*str);
+		str++;
+	}
+	_putchar('\n');
 }
